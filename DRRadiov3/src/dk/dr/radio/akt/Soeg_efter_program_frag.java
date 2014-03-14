c9f4352c7dc2fa171f38f8170c8014d9d78e0900
Jacob Nordfalk <nordfalk@lundogbendsen.dk>
2014-03-14 11:05:35 +0000

    Mere arbejde med levende ikon/notifikation/låseskærm

--- DRRadiov3/src/dk/dr/radio/akt/Soeg_efter_program_frag.java
+++ DRRadiov3/src/dk/dr/radio/akt/Soeg_efter_program_frag.java
@@ -1,7 +1,7 @@
 package dk.dr.radio.akt;
 
 //import android.R;
-import android.R.anim;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -41,206 +41,207 @@ import dk.dr.radio.v3.R;
 
 
 public class Soeg_efter_program_frag extends Basisfragment implements
-		OnClickListener, AdapterView.OnItemClickListener {
+    OnClickListener, AdapterView.OnItemClickListener {
 
-	private ListView listView;
-	private EditText søgFelt;
-	private ArrayList<Udsendelse> liste = new ArrayList<Udsendelse>();
-	protected View rod;
-	private ImageView søgKnap;
-	private String url;
-	private TextView tomStr;
+  private ListView listView;
+  private EditText søgFelt;
+  private ArrayList<Udsendelse> liste = new ArrayList<Udsendelse>();
+  protected View rod;
+  private ImageView søgKnap;
+  private String url;
+  private TextView tomStr;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		rod = inflater.inflate(R.layout.soeg_efter_program_frag/* kanal_frag */, container,
-				false);
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    rod = inflater.inflate(R.layout.soeg_efter_program_frag/* kanal_frag */, container,
+        false);
 
-		AQuery aq = new AQuery(rod);
-		listView = aq.id(R.id.listView).adapter(adapter).itemClicked(this)
-				.getListView();
-		listView.setEmptyView(aq.id(R.id.tom).typeface(App.skrift_gibson_fed)
-				.text("Søg efter program").getView());
+    AQuery aq = new AQuery(rod);
+    listView = aq.id(R.id.listView).adapter(adapter).itemClicked(this)
+        .getListView();
+    listView.setEmptyView(aq.id(R.id.tom).typeface(App.skrift_gibson_fed)
+        .text("Søg efter program").getView());
 
-		søgFelt = aq.id(R.id.soegFelt).getEditText();
-		søgFelt.setBackgroundResource(android.R.drawable.editbox_background_normal);
-		søgKnap = aq.id(R.id.soegKnap).clicked(this).getImageView();
-		søgKnap.setBackgroundResource(R.drawable.knap_graa_bg);
-		tomStr = aq.id(R.id.tom).getTextView();
+    søgFelt = aq.id(R.id.soegFelt).getEditText();
+    søgFelt.setBackgroundResource(android.R.drawable.editbox_background_normal);
+    søgKnap = aq.id(R.id.soegKnap).clicked(this).getImageView();
+    søgKnap.setBackgroundResource(R.drawable.knap_graa_bg);
+    tomStr = aq.id(R.id.tom).getTextView();
 
-		udvikling_checkDrSkrifter(rod, this + " rod");
-		/*
+    udvikling_checkDrSkrifter(rod, this + " rod");
+    /*
 		 * Kald
 		 * http://www.dr.dk/tjenester/mu-apps/search/programs?q=monte&type=radio
 		 * vil kun returnere radio programmer
 		 * http://www.dr.dk/tjenester/mu-apps/search/series?q=monte&type=radio
 		 * vil kun returnere radio serier
 		 */
-		// String url =
-		// "http://www.dr.dk/tjenester/mu-apps/search/programs?q=monte&type=radio";
+    // String url =
+    // "http://www.dr.dk/tjenester/mu-apps/search/programs?q=monte&type=radio";
 
-		// url = "http://www.dr.dk/tjenester/mu-apps/search/programs?q=" +
-		// søgStr + "&type=radio" ;
-		// new AQuery(App.instans).ajax(url, String.class, 1 * 60 * 60 * 1000,
-		// new AjaxCallback<String>() {
-		// @Override
-		// public void callback(String url, String json,
-		// AjaxStatus status) {
-		// App.sætErIGang(false);
-		// Log.d("XXX url " + url + "   status="
-		// + status.getCode());
-		// if (json != null && !"null".equals(json))
-		// try {
-		// JSONArray data = new JSONArray(json);
-		// Log.d("data = " + data.toString(2));
-		// liste = DRJson.parseUdsendelserForProgramserie(
-		// data, DRData.instans);
-		// Log.d("liste = " + liste);
-		// adapter.notifyDataSetChanged();
-		// return;
-		// } catch (Exception e) {
-		// Log.d("Parsefejl: " + e + " for json=" + json);
-		// e.printStackTrace();
-		// }
-		// new AQuery(rod).id(R.id.tom).text(
-		// url + "   status=" + status.getCode()
-		// + "\njson=" + json);
-		// }
-		// });
+    // url = "http://www.dr.dk/tjenester/mu-apps/search/programs?q=" +
+    // søgStr + "&type=radio" ;
+    // new AQuery(App.instans).ajax(url, String.class, 1 * 60 * 60 * 1000,
+    // new AjaxCallback<String>() {
+    // @Override
+    // public void callback(String url, String json,
+    // AjaxStatus status) {
+    // App.sætErIGang(false);
+    // Log.d("XXX url " + url + "   status="
+    // + status.getCode());
+    // if (json != null && !"null".equals(json))
+    // try {
+    // JSONArray data = new JSONArray(json);
+    // Log.d("data = " + data.toString(2));
+    // liste = DRJson.parseUdsendelserForProgramserie(
+    // data, DRData.instans);
+    // Log.d("liste = " + liste);
+    // adapter.notifyDataSetChanged();
+    // return;
+    // } catch (Exception e) {
+    // Log.d("Parsefejl: " + e + " for json=" + json);
+    // e.printStackTrace();
+    // }
+    // new AQuery(rod).id(R.id.tom).text(
+    // url + "   status=" + status.getCode()
+    // + "\njson=" + json);
+    // }
+    // });
 
-		return rod;
-	}
+    return rod;
+  }
 
-	/**
-	 * Viewholder designmønster - hold direkte referencer til de views og
-	 * objekter der bruges hele tiden
-	 */
-	private static class Viewholder {
-		public AQuery aq;
-		public TextView titel;
-		public TextView startid;
-		public Lydkilde lydkilde;
-	}
+  /**
+   * Viewholder designmønster - hold direkte referencer til de views og
+   * objekter der bruges hele tiden
+   */
+  private static class Viewholder {
+    public AQuery aq;
+    public TextView titel;
+    public TextView startid;
+    public Lydkilde lydkilde;
+  }
 
-	private BaseAdapter adapter = new Basisadapter() {
-		@Override
-		public int getCount() {
-			return liste.size();
-		}
+  private BaseAdapter adapter = new Basisadapter() {
+    @Override
+    public int getCount() {
+      return liste.size();
+    }
 
-		@Override
-		public View getView(int position, View v, ViewGroup parent) {
-			Viewholder vh;
-			AQuery a;
-			Lydkilde lydkilde = liste.get(position);
-			if (v == null) {
-				v = getLayoutInflater(null).inflate(
-						R.layout.udsendelse_elem2_tid_titel_kunstner, parent,
-						false);
-				vh = new Viewholder();
-				a = vh.aq = new AQuery(v);
-				vh.startid = a.id(R.id.startid).typeface(App.skrift_gibson)
-						.getTextView();
-				a.id(R.id.slutttid).gone();
+    @Override
+    public View getView(int position, View v, ViewGroup parent) {
+      Viewholder vh;
+      AQuery a;
+      Lydkilde lydkilde = liste.get(position);
+      if (v == null) {
+        v = getLayoutInflater(null).inflate(
+            R.layout.udsendelse_elem2_tid_titel_kunstner, parent,
+            false);
+        vh = new Viewholder();
+        a = vh.aq = new AQuery(v);
+        vh.startid = a.id(R.id.startid).typeface(App.skrift_gibson)
+            .getTextView();
+        a.id(R.id.slutttid).gone();
 
-				vh.titel = a.id(R.id.titel_og_kunstner)
-						.typeface(App.skrift_gibson_fed).getTextView();
+        vh.titel = a.id(R.id.titel_og_kunstner)
+            .typeface(App.skrift_gibson_fed).getTextView();
 
-				v.setTag(vh);
+        v.setTag(vh);
 
-			} else {
-				vh = (Viewholder) v.getTag();
-				a = vh.aq;
-			}
+      } else {
+        vh = (Viewholder) v.getTag();
+        a = vh.aq;
+      }
 
-			// Opdatér viewholderens data
-			vh.lydkilde = lydkilde;
+      // Opdatér viewholderens data
+      vh.lydkilde = lydkilde;
 
-			SimpleDateFormat ft = new SimpleDateFormat("HH:mm");
-			Date startTid = lydkilde.getUdsendelse().startTid;
-			
-			vh.startid.setText("" + ft.format(startTid));
+      SimpleDateFormat ft = new SimpleDateFormat("HH:mm");
+      Date startTid = lydkilde.getUdsendelse().startTid;
 
-			String titel = lydkilde.getUdsendelse().titel;
-			Spannable spannable = new SpannableString(titel);
-			spannable.setSpan(App.skrift_gibson_fed_span, 0, titel.length(),
-					Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-			vh.titel.setText(spannable);
+      vh.startid.setText("" + ft.format(startTid));
 
-			// vh.titel.setText(lydkilde.titel);
-			// a.id(R.id.stiplet_linje).visibility(position ==
-			// aktuelUdsendelseIndex + 1 ? View.INVISIBLE : View.VISIBLE);
-			// a.id(R.id.hør).visibility(lydkilde.kanHøres ? View.VISIBLE :
-			// View.GONE);
+      String titel = lydkilde.getUdsendelse().titel;
+      Spannable spannable = new SpannableString(titel);
+      spannable.setSpan(App.skrift_gibson_fed_span, 0, titel.length(),
+          Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+      vh.titel.setText(spannable);
 
-			udvikling_checkDrSkrifter(v, this.getClass() + " ");
+      // vh.titel.setText(lydkilde.titel);
+      // a.id(R.id.stiplet_linje).visibility(position ==
+      // aktuelUdsendelseIndex + 1 ? View.INVISIBLE : View.VISIBLE);
+      // a.id(R.id.hør).visibility(lydkilde.kanHøres ? View.VISIBLE :
+      // View.GONE);
 
-			return v;
-		}
-	};
-	private String søgStr;
+      udvikling_checkDrSkrifter(v, this.getClass() + " ");
 
-	@Override
-	public void onItemClick(AdapterView<?> listView, View v, int position,
-			long id) {
-		Lydkilde u = liste.get(position);
-		// startActivity(new Intent(getActivity(), VisFragment_akt.class)
-		// .putExtra(P_kode, kanal.kode)
-		// .putExtra(VisFragment_akt.KLASSE,
-		// Udsendelse_frag.class.getName()).putExtra(DRJson.Slug.name(),
-		// u.slug)); // Udsenselses-ID
+      return v;
+    }
+  };
+  private String søgStr;
 
-		Fragment f = new Udsendelse_frag();
-		f.setArguments(new Intent().putExtra(P_kode, u.kanal().kode)
-				.putExtra(DRJson.Slug.name(), u.slug).getExtras());
-		getActivity().getSupportFragmentManager().beginTransaction()
-				.replace(R.id.indhold_frag, f).addToBackStack(null)
-				.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-				.commit();
-	}
+  @Override
+  public void onItemClick(AdapterView<?> listView, View v, int position,
+                          long id) {
+    Lydkilde u = liste.get(position);
+    // startActivity(new Intent(getActivity(), VisFragment_akt.class)
+    // .putExtra(P_kode, kanal.kode)
+    // .putExtra(VisFragment_akt.KLASSE,
+    // Udsendelse_frag.class.getName()).putExtra(DRJson.Slug.name(),
+    // u.slug)); // Udsenselses-ID
 
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-	
-		søgStr = søgFelt.getText().toString();
+    Fragment f = new Udsendelse_frag();
+    f.setArguments(new Intent().putExtra(P_kode, u.kanal().kode)
+        .putExtra(DRJson.Slug.name(), u.slug).getExtras());
+    getActivity().getSupportFragmentManager().beginTransaction()
+        .replace(R.id.indhold_frag, f).addToBackStack(null)
+        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        .commit();
+  }
 
-		url = "http://www.dr.dk/tjenester/mu-apps/search/programs?q=" + søgStr
-				+ "&type=radio";
+  @Override
+  public void onClick(View v) {
+    // TODO Auto-generated method stub
 
-		new AQuery(App.instans).ajax(url, String.class, 1 * 60 * 60 * 1000,
-				new AjaxCallback<String>() {
-					@Override
-					public void callback(String url, String json,
-							AjaxStatus status) {
-						App.sætErIGang(false);
-						Log.d("XXX url " + url + "   status="
-								+ status.getCode());
-						if (json != null && !"null".equals(json))
-							try {
-								JSONArray data = new JSONArray(json);
-								Log.d("data = " + data.toString(2));
-								liste = DRJson.parseUdsendelserForProgramserie(
-										data, DRData.instans);
-								Log.d("liste = " + liste);
-								adapter.notifyDataSetChanged();
+    søgStr = søgFelt.getText().toString();
 
-								if (liste.size() == 0) {
-									tomStr.setText("Søgning giver ingen resultat!");
-								}
-								return;
-							} catch (Exception e) {
-								Log.d("Parsefejl: " + e + " for json=" + json);
-								e.printStackTrace();
-							}
-						new AQuery(rod).id(R.id.tom).text(
-								url + "   status=" + status.getCode()
-										+ "\njson=" + json);
-						Log.d("Slut søgning!");
-					}
+    url = "http://www.dr.dk/tjenester/mu-apps/search/programs?q=" + søgStr
+        + "&type=radio";
 
-				});
+    App.sætErIGang(true);
+    new AQuery(App.instans).ajax(url, String.class, 1 * 60 * 60 * 1000,
+        new AjaxCallback<String>() {
+          @Override
+          public void callback(String url, String json,
+                               AjaxStatus status) {
+            App.sætErIGang(false);
+            Log.d("XXX url " + url + "   status="
+                + status.getCode());
+            if (json != null && !"null".equals(json))
+              try {
+                JSONArray data = new JSONArray(json);
+                Log.d("data = " + data.toString(2));
+                liste = DRJson.parseUdsendelserForProgramserie(
+                    data, DRData.instans);
+                Log.d("liste = " + liste);
+                adapter.notifyDataSetChanged();
 
-	}
+                if (liste.size() == 0) {
+                  tomStr.setText("Søgning giver ingen resultat!");
+                }
+                return;
+              } catch (Exception e) {
+                Log.d("Parsefejl: " + e + " for json=" + json);
+                e.printStackTrace();
+              }
+            new AQuery(rod).id(R.id.tom).text(
+                url + "   status=" + status.getCode()
+                    + "\njson=" + json);
+            Log.d("Slut søgning!");
+          }
+
+        });
+
+  }
 }
