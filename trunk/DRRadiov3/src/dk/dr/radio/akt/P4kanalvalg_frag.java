95b73e567cd58d8d70a625f3042eb6b2e0eefba4
ucanreachjesper@gmail.com <ucanreachjesper@gmail.com@b4cc6702-63e9-9b9b-9f02-50805bb6e5c4>
2014-03-21 07:42:01 +0000

    Tilføjet ny skraldespand

--- trunk/DRRadiov3/src/dk/dr/radio/akt/P4kanalvalg_frag.java
+++ trunk/DRRadiov3/src/dk/dr/radio/akt/P4kanalvalg_frag.java
@@ -26,9 +26,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
+
 
 import java.util.ArrayList;
 import java.util.List;
