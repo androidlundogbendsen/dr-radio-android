65e2eae8132858355984e6c1cff5dea666adc204
nordfalk@lundogbendsen.dk <nordfalk@lundogbendsen.dk@b4cc6702-63e9-9b9b-9f02-50805bb6e5c4>
2014-05-21 21:23:08 +0000

    HLS: Start på at rense master.m3u8-filer for 'døde' servere - at hente master.m3u8-filen ned og starte en mikro-webserver der giver den lokalt (da MediaPlayer kun kan afspille HLS på URLer der starter med 'http') Diverse

--- trunk/DRRadiov3/src/dk/dr/radio/afspilning/MediaPlayerWrapper.java
+++ trunk/DRRadiov3/src/dk/dr/radio/afspilning/MediaPlayerWrapper.java
@@ -3,9 +3,23 @@ package dk.dr.radio.afspilning;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import java.io.FileInputStream;
-import java.io.IOException;
+import android.os.Environment;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+
+import dk.dr.radio.diverse.App;
+import dk.dr.radio.diverse.FilCache;
 import dk.dr.radio.diverse.Log;
 
 /**
@@ -44,7 +58,95 @@ public class MediaPlayerWrapper {
       fis.close();
       return;
     }
-    mediaPlayer.setDataSource(lydUrl);
+
+    if (lydUrl.endsWith("master.m3u8") && !App.fejlsøgning) {
+      if (!App.PRODUKTION) App.kortToast("Gemmer master.m3u8 lokalt og spiller den");
+
+      // Følgende dur ikke, URLen skal starte med http:// og slutte med  .m3u8
+      // se http://androidxref.com/4.0.4/xref/frameworks/base/media/libmediaplayerservice/MediaPlayerService.cpp#583
+      /*
+      //File f = File.createTempFile("afspil_","master.m3u8",App.instans.getFilesDir());
+      File f = new File(Environment.getExternalStorageDirectory(), "master.m3u8");
+      Log.d(f + " <<-- "+lydUrl);
+      InputStream is = new URL((lydUrl)).openStream();
+      FilCache.kopierOgLuk(is, new FileOutputStream(f));
+      FileInputStream fis = new FileInputStream(f);
+      mediaPlayer.setDataSource(fis.getFD());
+      fis.close();
+      f.delete();
+      */
+      InputStream is = new URL((lydUrl)).openStream();
+
+      final ByteArrayOutputStream bos = new ByteArrayOutputStream(1000);
+      /*
+HTTP/1.1 200 OK
+Server: AkamaiGHost
+Mime-Version: 1.0
+Content-Type: application/vnd.apple.mpegurl
+Content-Length: 596
+Expires: Wed, 21 May 2014 14:58:46 GMT
+Cache-Control: max-age=0, no-cache, no-store
+Pragma: no-cache
+Date: Wed, 21 May 2014 14:58:46 GMT
+Connection: keep-alive
+Set-Cookie: _alid_=WzFUnunxc/iaxEroCqOV3A==; path=/i/p1_9@143503/; domain=drradio1-lh.akamaihd.net
+
+#EXTM3U
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=64000,CODECS="mp4a.40.2"
+http://drradio1-lh.akamaihd.net/i/p1_9@143503/index_64_a-p.m3u8?sd=10&rebase=on
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=64000,CODECS="mp4a.40.2"
+http://drradio1-lh.akamaihd.net/i/p1_9@143503/index_64_a-b.m3u8?sd=10&rebase=on
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=192000,CODECS="mp4a.40.2"
+http://drradio1-lh.akamaihd.net/i/p1_9@143503/index_192_a-p.m3u8?sd=10&rebase=on
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=192000,CODECS="mp4a.40.2"
+http://drradio1-lh.akamaihd.net/i/p1_9@143503/index_192_a-b.m3u8?sd=10&rebase=on
+
+       */
+      FilCache.kopierOgLuk(is, bos);
+      bos.close();
+      final ServerSocket ss = new ServerSocket(0);
+      new Thread() {
+        @Override
+        public void run() {
+          try {
+            Log.d("Venter på socket "+ss);
+            Socket s = ss.accept();
+            Log.d("VI FIK EN socket "+s);
+            //int n = s.getInputStream().read(new byte[100]);
+            //Log.d("Læste " + n + " byte. Sender " + bos);
+            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            String l = br.readLine();
+            while (l!=null && l.length()>0) {
+              Log.d("Socket: "+l);
+              l = br.readLine();
+            }
+            Log.d("Sender " + bos.toString().replace('\n','/'));
+            OutputStream os = s.getOutputStream();
+            os.write(("HTTP/1.1 200 OK\n" +
+                "Content-Type: application/vnd.apple.mpegurl\n" +
+                "Content-Length: "+bos.size()+"\n" +
+                "Connection: close\n" +
+                "Cache-Control: max-age=0, no-cache, no-store\n" +
+                "Pragma: no-cache\n" +
+                "\n").getBytes());
+            os.write(bos.toByteArray());
+            ss.close();
+          } catch (IOException e) {
+            Log.rapporterFejl(e);
+          }
+        }
+      }.start();
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+
+      mediaPlayer.setDataSource("http://localhost:"+ss.getLocalPort()+"/master.m3u8");
+
+    } else {
+      mediaPlayer.setDataSource(lydUrl);
+    }
   }
 
   public void setAudioStreamType(int streamMusic) {
