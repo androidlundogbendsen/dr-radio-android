0cf28dde8113e93471fd7a69c115e38534d1ded3
Jacob Nordfalk <nordfalk@lundogbendsen.dk>
2014-10-24 13:16:17 +0000

    Udvidet afspiller er ‘modal’ - andet dimmes og den lukker hvis man trykker på andet. Tilbageknap lukker udvidet afspiller

--- DRRadiov3/src/dk/dr/radio/akt/Afspiller_frag.java
+++ DRRadiov3/src/dk/dr/radio/akt/Afspiller_frag.java
@@ -45,15 +45,29 @@ public class Afspiller_frag extends Basisfragment implements Runnable, View.OnCl
   private TextView metainformation;
   private ImageView udvidSkjulKnap;
   private View udvidSkjulOmråde;
+  private View rod;
+  private View indhold_overskygge;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     Log.d("Viser fragment " + this);
-    View rod = inflater.inflate(R.layout.afspiller_frag, container, false);
+    rod = inflater.inflate(R.layout.afspiller_frag, container, false);
     aq = new AQuery(rod);
     rod.setOnClickListener(this); // Ved klik på baggrunden skal kanalforside eller aktuel udsendelsesside vises
     startStopKnap = aq.id(R.id.startStopKnap).clicked(this).getImageView();
-    udvidSkjulKnap = aq.id(R.id.udvidSkjulKnap).clicked(this).getImageView();
+    udvidSkjulKnap = aq.id(R.id.udvidSkjulKnap).getImageView();
+    // udvid/skjul knap - hvis vi bruger en onClickListener får vi først  besked når knappen slippes.
+    // I stedet viser/skjuler vi allerede når fingeren trykkes ned på viewet
+    udvidSkjulKnap.setOnTouchListener(new View.OnTouchListener() {
+      @Override
+      public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+          udvidSkjulOmråde();
+        }
+        return false;
+      }
+    });
+
     udvidSkjulOmråde = aq.id(R.id.udvidSkjulOmråde).gone().getView();
     progressbar = aq.id(R.id.progressBar).getProgressBar();
     titel = aq.id(R.id.titel).typeface(App.skrift_gibson_fed).getTextView();
@@ -74,8 +88,11 @@ public class Afspiller_frag extends Basisfragment implements Runnable, View.OnCl
         ((View) startStopKnap.getParent()).setTouchDelegate(new TouchDelegate(r, startStopKnap));
       }
     });
+    aq.id(R.id.forrige).clicked(this);
+    aq.id(R.id.næste).clicked(this);
     DRData.instans.afspiller.observatører.add(this);
     DRData.instans.afspiller.forbindelseobservatører.add(this);
+    DRData.instans.afspiller.positionsobservatører.add(this);
     run(); // opdatér views
 
     if (App.accessibilityManager.isEnabled()) setHasOptionsMenu(true);
@@ -86,6 +103,7 @@ public class Afspiller_frag extends Basisfragment implements Runnable, View.OnCl
   public void onDestroyView() {
     DRData.instans.afspiller.observatører.remove(this);
     DRData.instans.afspiller.forbindelseobservatører.remove(this);
+    DRData.instans.afspiller.positionsobservatører.remove(this);
     super.onDestroyView();
   }
 
@@ -200,43 +218,110 @@ public class Afspiller_frag extends Basisfragment implements Runnable, View.OnCl
     return udvidSkjulOmråde.getVisibility() == View.VISIBLE;
   }
 
+  /*
+  Virkede med
+      android:animateLayoutChanges="true"
 
-  public void udvidSkjulOmråde() {
-    final View indhold_overskygge = getActivity().findViewById(R.id.indhold_overskygge);
-    if (viserUdvidetOmråde()) {
-      udvidSkjulOmråde.setVisibility(View.GONE);
-      udvidSkjulKnap.setImageResource(R.drawable.dri_pil_op_graa40);
 
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-        indhold_overskygge.animate().alpha(0).withEndAction(new Runnable() {
+    public void udvidSkjulOmråde() {
+      final View indhold_overskygge = getActivity().findViewById(R.id.indhold_overskygge);
+      if (viserUdvidetOmråde()) {
+        udvidSkjulOmråde.setVisibility(View.GONE);
+        udvidSkjulKnap.setImageResource(R.drawable.dri_pil_op_graa40);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+          indhold_overskygge.animate().alpha(0).withEndAction(new Runnable() {
+            @Override
+            public void run() {
+              // Får det til at hakke midt i animationen
+              // TODO fix, ved at lave egen åbne/lukke animation i afspiller, der ikke først fader og DEREFTER krympler området
+              //indhold_overskygge.setVisibility(View.GONE);
+            }
+          });
+        } else {
+          indhold_overskygge.setVisibility(View.GONE);
+        }
+      } else {
+        udvidSkjulOmråde.setVisibility(View.VISIBLE);
+        udvidSkjulKnap.setImageResource(R.drawable.dri_pil_ned_graa40);
+
+        indhold_overskygge.setOnTouchListener(new View.OnTouchListener() {
           @Override
-          public void run() {
-            // Får det til at hakke midt i animationen
-            // TODO fix, ved at lave egen åbne/lukke animation i afspiller, der ikke først fader og DEREFTER krympler området
-            //indhold_overskygge.setVisibility(View.GONE);
+          public boolean onTouch(View v, MotionEvent event) {
+            udvidSkjulOmråde(); // Skjul udvidet afspiller igen
+            indhold_overskygge.setOnTouchListener(null);
+            return true;
           }
         });
+        indhold_overskygge.setVisibility(View.VISIBLE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+          indhold_overskygge.animate().alpha(1);
+        }
+      }
+    }
+  */
+  public void setIndholdOverskygge(View v) {
+    indhold_overskygge = v;
+    indhold_overskygge.setVisibility(View.GONE);
+  }
+
+  View.OnTouchListener indhold_overskygge_onTouchListener = new View.OnTouchListener() {
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+      udvidSkjulOmråde(); // Skjul udvidet afspiller igen
+      return true;
+    }
+  };
+
+  public void udvidSkjulOmråde() {
+    if (!viserUdvidetOmråde()) {
+      indhold_overskygge.setOnTouchListener(indhold_overskygge_onTouchListener);
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+        indhold_overskygge.setVisibility(View.VISIBLE);
+        udvidSkjulOmråde.setVisibility(View.VISIBLE);
+        int højde = udvidSkjulOmråde.getHeight();
+        //App.kortToast("højde " + højde);
+        int højdeGæt = getResources().getDimensionPixelSize(R.dimen.afspiller_udvidet_højde_gæt);
+        if (højde == 0) {
+          //udvidSkjulKnap.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+          højde = højdeGæt;
+          //App.kortToast("højde "+højde);
+        } else {
+          if (højdeGæt != højde) {
+            Log.d("udvidSkjulOmråde(): højdeGæt på " + højdeGæt + " afviger fra reel højde på " + højde);
+          }
+        }
+        rod.setTranslationY(højde);
+        rod.animate().translationY(0);
+        indhold_overskygge.animate().alpha(1);
+        udvidSkjulKnap.animate().rotation(-180);
       } else {
-        indhold_overskygge.setVisibility(View.GONE);
+        udvidSkjulKnap.setImageResource(R.drawable.dri_pil_ned_graa40);
+        indhold_overskygge.setVisibility(View.VISIBLE);
+        udvidSkjulOmråde.setVisibility(View.VISIBLE);
       }
     } else {
-      udvidSkjulOmråde.setVisibility(View.VISIBLE);
-      udvidSkjulKnap.setImageResource(R.drawable.dri_pil_ned_graa40);
-
-      indhold_overskygge.setOnTouchListener(new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-          udvidSkjulOmråde(); // Skjul udvidet afspiller igen
-          indhold_overskygge.setOnTouchListener(null);
-          return true;
-        }
-      });
-      indhold_overskygge.setVisibility(View.VISIBLE);
+      indhold_overskygge.setOnTouchListener(null);
       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-        indhold_overskygge.animate().alpha(1);
+        rod.animate().translationY(udvidSkjulOmråde.getHeight());
+        indhold_overskygge.animate().alpha(0).withEndAction(new Runnable() {
+          @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+          @Override
+          public void run() {
+            indhold_overskygge.setVisibility(View.GONE);
+            udvidSkjulOmråde.setVisibility(View.GONE);
+            rod.setTranslationY(0);
+          }
+        });
+        udvidSkjulKnap.animate().rotation(0);
+      } else {
+        udvidSkjulKnap.setImageResource(R.drawable.dri_pil_op_graa40);
+        udvidSkjulOmråde.setVisibility(View.GONE);
+        indhold_overskygge.setVisibility(View.GONE);
       }
     }
   }
+
 
 
   @Override
@@ -247,6 +332,10 @@ public class Afspiller_frag extends Basisfragment implements Runnable, View.OnCl
       } else {
         DRData.instans.afspiller.stopAfspilning();
       }
+    } else if (v.getId() == R.id.forrige) {
+      DRData.instans.afspiller.forrige();
+    } else if (v.getId() == R.id.næste) {
+      DRData.instans.afspiller.næste();
     } else if (v == udvidSkjulKnap) {
       udvidSkjulOmråde();
     } else try {
