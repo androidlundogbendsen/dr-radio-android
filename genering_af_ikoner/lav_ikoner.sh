789c0c9fb39adb1e8511c802b117ec6ad74b24f1
Jacob Nordfalk <nordfalk@lundogbendsen.dk>
2014-12-03 12:19:06 +0000

    Nyt ikonsæt

--- genering_af_ikoner/lav_ikoner.sh
+++ genering_af_ikoner/lav_ikoner.sh
@@ -4,9 +4,10 @@
 odt2txt ikoner_stamdokument.odt | while read l; 
 do 
 	i=`echo $l|cut -f1 -d' '`; 
-	n=`echo $l|cut -f2 -d' '`; 
+	n=`echo $l|cut -f2 -d' '|tr '[:upper:]' '[:lower:]'`; 
 	if [ "$n" = "x" -o "x$n" = "x" ]; then continue; fi; 
-	echo -n "$i $n  ->  "; 
+	echo -n "'$i' '$n'  ->  "; 
+	if [[ $n != dri_* ]]; then n=dri_$n; fi; 
 	
 #	for dpi in mdpi hdpi xhdpi xxhdpi; 
 	for dpi in xhdpi; 
@@ -22,16 +23,17 @@ do
 #		xp=512
 #		dpi=ios
 
-		sti=../DRRadiov3/res/drawable-${dpi}
-#		sti=res/drawable-${dpi}
+		sti=../DRRadiov3/resny/drawable-${dpi}
 #		sti=res/drawable-${dpi}
 		mkdir -p $sti
-		konv="convert -background none -font dr-icons-Regular -size ${xpi}x${xpi} -gravity center -trim -extent ${xp}x${xp}"
+#		konv="convert -background none -font dr-icons-Regular -size ${xpi}x${xpi} -gravity center -trim -extent ${xp}x${xp}"
+		konv="convert -background none -font icomoon -size ${xpi}x${xpi} -gravity center -trim -extent ${xp}x${xp}"
 
 #		fn="$sti/${n}.png"; $konv -fill black label:"$i" $fn; 
-		fn="$sti/${n}_blaa.png"; $konv -fill '#55b9c4' label:"$i" $fn; 
-		fn="$sti/${n}_graa40.png"; $konv -fill '#999999' label:"$i" $fn; 
-		fn="$sti/${n}_hvid.png";   $konv -fill white label:"$i" $fn; 
+		echo $konv -fill '#55b9c4' label:"$i" $fn; 
+		fn="$sti/${n}_blaa.png"; $konv -fill '#55b9c4' label:"\\$i" $fn; 
+		fn="$sti/${n}_graa40.png"; $konv -fill '#999999' label:"\\$i" $fn; 
+		fn="$sti/${n}_hvid.png";   $konv -fill white label:"\\$i" $fn; 
 		echo -n "  $fn"
 
 	done
