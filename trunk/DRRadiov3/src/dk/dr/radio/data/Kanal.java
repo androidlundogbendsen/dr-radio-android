cc129eef6336e26a62af13c525b469e23a012a30
nordfalk@lundogbendsen.dk <nordfalk@lundogbendsen.dk@b4cc6702-63e9-9b9b-9f02-50805bb6e5c4>
2015-04-01 22:11:21 +0000

    Småt. Kanal specificerer hvilken fragmentklasse der skal bruges til at vise den

--- trunk/DRRadiov3/src/dk/dr/radio/data/Kanal.java
+++ trunk/DRRadiov3/src/dk/dr/radio/data/Kanal.java
@@ -18,6 +18,8 @@
 
 package dk.dr.radio.data;
 
+import android.support.v4.app.Fragment;
+
 import org.json.JSONException;
 
 import java.text.ParseException;
@@ -41,6 +43,7 @@ public class Kanal extends Lydkilde {
   public transient SortedMap<String, ArrayList<Udsendelse>> udsendelserPerDag = new TreeMap<String, ArrayList<Udsendelse>>();
   /** P1 har ingen senest spillet og der er aldrig playlister på denne kanal */
   public boolean ingenPlaylister;
+  public Class<? extends Fragment> fragKlasse;
 
   @Override
   public String toString() {
